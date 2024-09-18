fcea18ae188a8de28972fa2f37563e05402561f3
Scerka <sceri73@gmail.com>
2024-09-18 20:50:03 +0400

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -134,7 +134,7 @@ const config: Config = {
 			],
 		},
 	  ],
-      copyright: `Copyright © ${new Date().getFullYear()} <a href="//github.com/scerka-soft" target="_blank" rel="noopener noreferrer">SCERKA SOFT</a>.<br>Создано с помощью <a href="//docusaurus.io/" target="_blank" rel="noopener noreferrer">Docusaurus</a>.`,
+      copyright: `Copyright © ${new Date().getFullYear()} <a href="//github.com/scerka-soft" target="_blank" rel="noopener noreferrer">SCERKA SOFT</a>.<br>Создано с помощью <a href="//docusaurus.io/" target="_blank" rel="noopener noreferrer">Docusaurus</a>.<script>let element = document.getElementById("search_input_react");let attr = element.getAttribute("placeholder");element.setAttribute('placeholder', attr.replace('Search', 'Поиск'));</script>`,
     },
     prism: {
       theme: prismThemes.github,
