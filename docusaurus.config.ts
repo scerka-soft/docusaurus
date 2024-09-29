c40c8a6b1ac163ccae4f131be8eacb9b275a2397
Scerka <sceri73@gmail.com>
2024-09-29 21:33:59 +0400

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,9 +17,6 @@ const config: Config = {
   organizationName: 'scerka-soft',
   projectName: 'docusaurus',
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
   i18n: {
     defaultLocale: 'ru',
     locales: ['ru'],
