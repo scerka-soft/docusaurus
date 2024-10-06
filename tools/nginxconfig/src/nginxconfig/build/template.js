5c6e4dbe97f30300b3c06ddda4d670a83cc1bdf9
Scerka <sceri73@gmail.com>
2024-10-06 20:32:39 +0400

    update

--- tools/nginxconfig/src/nginxconfig/build/template.js
+++ tools/nginxconfig/src/nginxconfig/build/template.js
@@ -23,6 +23,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+import fs from 'fs';
+import { URL } from 'url';
 
 // Fetch the posthtml template and convert it to an ejs template
 const main = () => {
